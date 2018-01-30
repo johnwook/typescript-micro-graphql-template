ef29871e7cf183202e8be8cae4d241c9d3f71b82
Johnwook Choi <johnwook.choi@huiseoul.com>
2018-01-30 22:32:29 +0900

    make git ignore generated js files

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-module.exports = (req, res) => {
-  res.end("Welcome to Micro");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const handler = (_, res) => {
+    res.end("Welcome to Micro");
 };
+module.exports = handler;
